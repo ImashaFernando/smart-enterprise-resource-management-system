98aa038a129fdb0f8b01d353a598d071565ca29e
ImashaFernando <imashaudari412@gmail.com>
2026-07-07 23:41:10 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/serms/SmartEnterpriseResourceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.serms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEnterpriseResourceManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEnterpriseResourceManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serms/SmartEnterpriseResourceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEnterpriseResourceManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
