239d5a0d4d3cf028c1b58a9e8281c050bfd307c2
ImashaFernando <imashaudari412@gmail.com>
2026-07-26 01:19:01 +0530

    Complete Finance Management module

--- src/main/java/com/serms/controller/FinanceController.java
+++ src/main/java/com/serms/controller/FinanceController.java
@@ -1,4 +1,32 @@
 package com.serms.controller;
 
+import com.serms.entity.Finance;
+import com.serms.service.FinanceService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/finance")
 public class FinanceController {
+
+    @Autowired
+    private FinanceService financeService;
+
+    @GetMapping
+    public String listFinance(@RequestParam(required = false) String keyword, Model model) {
+
+        model.addAttribute("financeList", financeService.searchFinance(keyword));
+        model.addAttribute("keyword", keyword);
+
+        return "finance";
+    }
+
+    @GetMapping("/add")
+    public String addFinance(Model model) {
+        model.addAttribute("finance", new Finance());
+        return "finance-add";
+    }
 }
